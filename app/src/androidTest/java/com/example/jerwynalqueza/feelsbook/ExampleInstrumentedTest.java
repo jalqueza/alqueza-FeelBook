601571029e0d810ef95d71533d5bb3e74fa17607
jalqueza <jerwynalqueza@gmail.com>
2018-10-03 12:08:46 -0600

    update test

--- app/src/androidTest/java/com/example/jerwynalqueza/feelsbook/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/jerwynalqueza/feelsbook/ExampleInstrumentedTest.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
@@ -27,36 +29,19 @@ public class ExampleInstrumentedTest {
     }
 
     public void testEmotion() {
-        Emotion love = new Emotion("Love", "❤");
+        Emotion love = new Emotion("Love");
         assertTrue("Name isn't Correct", "Love".equals(love.getName()));
-        assertTrue("Emote isn't Correct", "❤".equals(love.getEmoji()));
         addEntryToList(love);
     }
 
     public void addEntryToList(Emotion emotion) {
 
         EmotionList emotionList = new EmotionList();
-
         assertTrue("Love isn't in EmotionList", emotionList.getEmotionList().get(0).getName().equals("Love"));
-        assertTrue("Fear Emoji isn't in EmotionList", emotionList.getEmotionList().get(5).getEmoji().equals("\uD83D\uDE28"));
 
-        String string = "I'm in love";
-        Entry newEntry0 = new Entry("Love", "January 2 2019", string, emotionList.getEmotionList());
-        Entry newEntry1 = new Entry("Love", "January 3 2019", "I'm in love1", emotionList.getEmotionList());
-        Entry newEntry2 = new Entry("Love", "January 4 2019", "I'm in love2", emotionList.getEmotionList());
-        newEntry0.addCommentDate();
-        newEntry1.addCommentDate();
-        newEntry2.addCommentDate();
-
-        assertTrue("Entry count isn't updated", emotionList.getEmotionList().get(0).getCount() == 3);
-        newEntry1.deleteCommentDate();
-        assertTrue("Entry count isn't updated", emotionList.getEmotionList().get(0).getCount() == 2);
-        assertTrue("First comment is inaccurate", emotionList.getEmotionList().get(0).getComments().get(0).equals("I'm in love"));
-        assertTrue("Second comment is inaccurate", emotionList.getEmotionList().get(0).getComments().get(1).equals("I'm in love2"));
-        assertTrue("commentList size isn't accurate", emotionList.getEmotionList().get(0).getComments().size() == 2);
-
-        newEntry2.editComment("YOOOOO");
-        assertTrue("Entry1 comment wasn't changed", emotionList.getEmotionList().get(0).getComments().get(1).equals("YOOOOO"));
-
+        EntryList entryList = new EntryList();
+        Entry entry = new Entry("Love", new Date(),"In love");
+        entryList.addEntryToList(entry);
+        assertTrue("Comment isn't correct", entryList.getEntryList().get(0).getComment().equals("In love"));
     }
 }
